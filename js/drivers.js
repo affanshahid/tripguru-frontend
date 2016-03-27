7c38858aa0dd0bccf926ee5a0ed8756ff33e64bb
Affan Shahid <affan.shahid.94@gmail.com>
2016-03-28 01:39:54 +0500

    Add drivers page

---
+++ js/drivers.js
@@ -0,0 +1,32 @@
+/*globals Handlebars*/
+$(function() {
+    var driverTemplate = Handlebars.compile($('#driver-template').html());
+    var driverTable = $('#driver-table');
+    var refreshButton = $('#refresh');
+
+    refreshButton.click(function() {
+        refreshButton.attr('disabled', 'disabled');
+        refreshDrivers(function() {
+            refreshButton.removeAttr('disabled');
+        });
+    });
+
+    refreshButton.click();
+
+    function refreshDrivers(cb) {
+        $.get('/drivers', function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var driver = data[i];
+                driver.age = (new Date()).getFullYear() - driver.dob.getFullYear();
+                var $driver = $(driverTemplate(driver));
+                $driver.hide();
+                driverTable.append($driver);
+                $driver.fadeIn('slow');
+            }
+            cb();
+        }).fail(function() {
+            cb();
+        });
+    }
+});
+
